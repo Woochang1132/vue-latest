2bd43d00f9fe48f11e2ba2c275ad57700c036be8
Woochang1132 <show4031132@gmail.com>
2025-02-22 15:39:53 +0900

    [vue-;atest] vue3.3 > defineOptions(), toValue(), toRef()

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,34 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import BaseCard from '@/components/BaseCard.vue'
+import { useFetch } from '@/composables/useFetch.js'
+import { ref } from 'vue'
+
+// URL을 ref로 선언하여 유연하게 사용할 수 있음
+const todoUrl = ref('/todos.json')
+const { data: todos } = useFetch(todoUrl)
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <main class="bg-gray-50 min-h-screen p-16">
+    <BaseCard custom-attr="Hello World!">
+      <h1 class="text-3xl font-bold text-center mb-8">할 일 목록</h1>
+      <div class="space-y-2">
+        <div
+          v-for="todo in todos"
+          :key="todo.id"
+          class="p-4 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 transition-all"
+          draggable="true"
+        >
+          <span
+            class="text-gray-800"
+            :class="{
+              'line-through text-gray-400': todo.completed,
+            }"
+          >
+            {{ todo.title }}
+          </span>
+        </div>
+      </div>
+    </BaseCard>
+  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
