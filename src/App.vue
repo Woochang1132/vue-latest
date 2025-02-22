78220fe73ac7533e4a6e2040a76ce70fc0a459b1
Woochang1132 <show4031132@gmail.com>
2025-02-22 16:49:13 +0900

    [vue-latest] vue3.4 defineModel() 적용해보기

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,49 @@
 <script setup>
 import BaseCard from '@/components/BaseCard.vue'
+import BaseInput from './components/BaseInput.vue'
 import { useFetch } from '@/composables/useFetch.js'
 import { ref } from 'vue'
 
 // URL을 ref로 선언하여 유연하게 사용할 수 있음
 const todoUrl = ref('/todos.json')
 const { data: todos } = useFetch(todoUrl)
+
+const newTodo = ref('')
+const handleAddTodo = () => {
+  todos.value.push({
+    id: Date.now(),
+    title: newTodo.value,
+    completed: false,
+  })
+}
 </script>
 
 <template>
   <main class="bg-gray-50 min-h-screen p-16">
     <BaseCard custom-attr="Hello World!">
       <h1 class="text-3xl font-bold text-center mb-8">할 일 목록</h1>
-      <div class="space-y-2">
-        <div
-          v-for="todo in todos"
-          :key="todo.id"
-          class="p-4 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 transition-all"
-          draggable="true"
-        >
-          <span
-            class="text-gray-800"
-            :class="{
-              'line-through text-gray-400': todo.completed,
-            }"
+      <div class="space-y-4">
+        <BaseInput
+          v-model="newTodo"
+          @keyup.ctrl.enter="handleAddTodo"
+        ></BaseInput>
+
+        <div class="space-y-2">
+          <div
+            v-for="todo in todos"
+            :key="todo.id"
+            class="p-4 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 transition-all"
+            draggable="true"
           >
-            {{ todo.title }}
-          </span>
+            <span
+              class="text-gray-800"
+              :class="{
+                'line-through text-gray-400': todo.completed,
+              }"
+            >
+              {{ todo.title }}
+            </span>
+          </div>
         </div>
       </div>
     </BaseCard>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <input
+    v-model="model"
+    type="text"
+    placeholder="Add a new task..."
+    class="w-full px-4 py-3 border border-gray-300 rounded-lg"
+  />
+</template>
+
+<script setup>
+const model = defineModel()
+</script>
+
+<style lang="scss" scoped></style>
